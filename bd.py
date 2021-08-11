7bca57f03457e491d01a883f5a334a502e3fa325
AlejoErwin <alejo.gutierrez.erwin@gmail.com>
2021-08-11 00:26:44 -0400

    select de la lista

--- bd.py
+++ bd.py
@@ -7,7 +7,17 @@ class DataBase:
             password='',
             db='indice'
         )
-        self.cursos = self.connection.cursor()
+        self.cursor = self.connection.cursor()
         print("Conexion establecida exitosamente!")
 
+    def select_user(self, id):
+        print(id)
+        sql= 'SELECT id,titulo,autor FROM context'
+        try:
+            self.cursor.execute(sql)
+            user = self.cursor.fetchall()
+            print(user)
+        except Exception as e:
+            raise
 database = DataBase()
+database.select_user(1)
